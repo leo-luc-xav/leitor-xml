8a01441de4a443f958abe59150b90b4e9a5e8668
Leonardo <leoxavier.comercial@gmail.com>
2026-08-21 15:52:19 -0300

    nota de venda á está obtendo as informações

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import xmltodict
 import re
 
 # 1. TÍTULO DA PÁGINA
-st.title("Extrator de XML - NFSe 📄")
+st.title("Extrator de XML 📄")
 st.write("Faça o upload da nota fiscal para extrair e validar os dados.")
 
 # 2. BOTÃO DE UPLOAD (Substitui o 'with open(...)')
@@ -40,7 +40,6 @@ if arquivo_xml is not None:
             resto_dividido = resto.split('.', 1)
             informacoes['nome_escola'] = resto_dividido[0].strip()
             informacoes['endereco_escola'] = resto_dividido[1].strip() if len(resto_dividido) > 1 else ''
-        # --- FIM DO SEU CÓDIGO ---
     
     
         st.divider() # Cria uma linha horizontal na tela para separar
@@ -59,7 +58,7 @@ if arquivo_xml is not None:
             with col2:
                 data_editada = st.text_input("Data de Emissão", value=informacoes['data_emissao'])
             with col3:
-                valor_editado = st.text_input("Valor (R$)", value=informacoes['valor_servicos'])
+                valor_editado = st.text_input("Valor Total (R$)", value=informacoes['valor_servicos'])
     
             # st.text_area é melhor para textos longos
             desc_editada = st.text_area("Descrição do Serviço", value=informacoes['descricao_serv'])
@@ -77,30 +76,45 @@ if arquivo_xml is not None:
     elif 'NFe' in data or 'nfeProc' in data:
         st.success("Nota de Produto carregada com Sucesso!")
 
+        #obtem as informações da escola a partir das informaçoes complementares 
         info = data['nfeProc']['NFe']['infNFe']
         pcrj = r"PCRJ/E/CRE \([\d.]+\)"
         corte = 'DOCUMENTO EMITIDO POR ME OU EPP OPTANTE PELO SIMPLES NACIONAL. NAO GERA DIREITO A CREDITO FISCAL DE ICMS, ISS E IPI'
         infos_escola = re.sub(pcrj, "", info['infAdic']['infCpl']).replace(corte, "")
+        nome = infos_escola.split('.', 1)[0]
+        endereco = infos_escola.split('.', 1)[1] if len(infos_escola.split('.', 1)) > 1 else ''
 
-        st.success(infos_escola)
-    
+        #obtendo informações dos produtos. Caso seja mais de um ele considera lista, para tratar isso estamos verificando se ele é um dicionário, caso seja, transformamos em lista para manter a consistência do código
+        detalhes = info['det']
+        if type(detalhes) is dict:
+            detalhes = [detalhes] 
+
+        #vamos percorrer a lista, armazenando as informações que queremos na lista com os produtos
+        lista_produtos = []
+        for det in detalhes:
+            produto_xml = det['prod']
+
+            produto_limpo = {
+                'código': produto_xml['cProd'],
+                'descrição': produto_xml['xProd'],
+                'quantidade': produto_xml['qCom'],
+                'valor_unitário': produto_xml['vUnCom'],
+                'valor_total': produto_xml['vProd']
+            }
+            lista_produtos.append(produto_limpo)
+
+
+
         informacoes = {
-            'numero': info['nNFSe'],
-            'data_emissao': info['DPS']['infDPS']['dhEmi'].split('T')[0],
-            'valor_servicos': info['valores']['vBC'],
-            'descricao_serv': '',
-            'nome_escola': '',
-            'endereco_escola': ''
+            'numero': info['ide']['nNF'],
+            'data_emissao': info['ide']['dhEmi'].split('T')[0],
+            'valor_servicos': info['total']['ICMSTot']['vNF'],
+            'nome_escola': nome,
+            'endereco_escola': endereco
         }
-    
-        if len(desc_partes) > 1:
-            informacoes['descricao_serv'] = desc_partes[0].strip()
-            resto = desc_partes[1].strip()
-            resto_dividido = resto.split('.', 1)
-            informacoes['nome_escola'] = resto_dividido[0].strip()
-            informacoes['endereco_escola'] = resto_dividido[1].strip() if len(resto_dividido) > 1 else ''
-        # --- FIM DO SEU CÓDIGO ---
-    
+
+        #st.success(informacoes)
+        #st.success(lista_produtos)
     
         st.divider() # Cria uma linha horizontal na tela para separar
         st.subheader("Verifique e edite as informações:")
@@ -118,16 +132,29 @@ if arquivo_xml is not None:
             with col2:
                 data_editada = st.text_input("Data de Emissão", value=informacoes['data_emissao'])
             with col3:
-                valor_editado = st.text_input("Valor (R$)", value=informacoes['valor_servicos'])
+                valor_editado = st.text_input("Valor Total  (R$)", value=informacoes['valor_servicos'])
     
-            # st.text_area é melhor para textos longos
-            desc_editada = st.text_area("Descrição do Serviço", value=informacoes['descricao_serv'])
-            
-            escola_editada = st.text_input("Nome da Escola", value=informacoes['nome_escola'])
-            end_editado = st.text_input("Endereço da Escola", value=informacoes['endereco_escola'])
-    
+            col1, col2 = st.columns(2) # Divide a tela em 2 colunas para ficar bonito
+
+            with col1:
+                escola_editada = st.text_input("Nome da Escola", value=informacoes['nome_escola'])
+            with col2:
+                end_editado = st.text_input("Endereço da Escola", value=informacoes['endereco_escola'])
+
+            st.divider()
+            st.subheader("Verifique e edite os produtos:")
+            st.write("Clique duas vezes em qualquer célula para editar.")
+
+            # O st.data_editor cria a tabela e salva o resultado editado na variável
+            produtos_editados = st.data_editor(
+                lista_produtos,
+                use_container_width=True, # Faz a tabela ocupar toda a largura da tela
+                num_rows="dynamic"        # Permite ao usuário adicionar ou excluir linhas!
+            )   
+
             # Botão de salvar
             botao_salvar = st.form_submit_button("Confirmar e Salvar")
+        
     
         # 5. O QUE ACONTECE QUANDO ELE CLICA EM SALVAR?
         if botao_salvar:
